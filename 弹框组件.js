04ebe7e8dd68e426b117992f459c181c92fa6a0e
pigyun <814728667@qq.com>
2017-12-27 13:41:20 +0800

    bian

--- 弹框组件.js
+++ 弹框组件.js
@@ -1,1 +1,26 @@
-var inputs = document.getElementsByTagName('input');
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (val, index) {
+	inputs[index].onclick =function () {
+		box();
+		var divbox = document.getElementById('divbox');
+		divbox.style.position = "fixed";
+		divbox.style.top = "30%";
+		divbox.style.left = "40*index"+"%";
+		console.log(divbox.style.left);
+		console.log(index);
+	};
+})
+
+function box () {
+	var container = document.getElementById('container');
+	console.log(container);
+	var divbox = document.createElement("div");
+	divbox.setAttribute("id","divbox");
+	container.appendChild(divbox);
+  divbox.innerHTML = '<div class="divtop">'
+  +'<button class="bottonright" id="close">×</button>'
+  +'</div>'
+  +'<p>hello, i am pang pang yun, i am pang pang yun, i am pang pang yun, i am pang pang yun</p>'
+  +'<button class="bottonb" id="confirm">确定</button>'
+  console.log(divbox);
+}
