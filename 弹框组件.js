6d9e0cc7973c2567ba84c71299c313b4f9be33f4
pigyun <814728667@qq.com>
2017-12-27 17:26:11 +0800

    bian2

--- 弹框组件.js
+++ 弹框组件.js
@@ -1,26 +1,67 @@
 var inputs = document.querySelectorAll('input');
 inputs.forEach(function (val, index) {
 	inputs[index].onclick =function () {
-		box();
-		var divbox = document.getElementById('divbox');
-		divbox.style.position = "fixed";
-		divbox.style.top = "30%";
-		divbox.style.left = "40*index"+"%";
-		console.log(divbox.style.left);
-		console.log(index);
+		createbox();
+	  var divboxs = document.querySelectorAll('.divbox');
+	  divboxs[index].setAttribute("id","divbox"+index);
+	  inputs[index].setAttribute("disabled","disabled");
+	  var divtoptext = document.querySelectorAll('.divtoptext');
+	  divtoptext[index].innerText = "这是第" + (index+1) + "个弹框";
 	};
 })
 
-function box () {
-	var container = document.getElementById('container');
+var container = document.getElementById('container');
+function createbox () {
 	console.log(container);
 	var divbox = document.createElement("div");
-	divbox.setAttribute("id","divbox");
+	divbox.className ="divbox";
 	container.appendChild(divbox);
   divbox.innerHTML = '<div class="divtop">'
-  +'<button class="bottonright" id="close">×</button>'
+  +'<div class="divtoptext"></div>'
+  +'<button class="bottonright">×</button>'
   +'</div>'
   +'<p>hello, i am pang pang yun, i am pang pang yun, i am pang pang yun, i am pang pang yun</p>'
-  +'<button class="bottonb" id="confirm">确定</button>'
+  +'<button class="bottonb1">取消</button>'
+  +'<button class="bottonb2">确定</button>'
   console.log(divbox);
 }
+
+//有问题
+function close () {
+	var bottonrights = document.querySelectorAll('.bottonright');
+  bottonrights.forEach(function (val, index) {
+  	var divboxs = document.querySelectorAll(".divbox");
+  	bottonrights[index].onclick = function () {
+  		container.removeChild(divboxs[index]);
+  		console.log(container);
+  	}
+  })
+  var bottonb1s = document.querySelectorAll('.bottonb1');
+  bottonb1s.forEach(function (val, index) {
+  	var divboxs = document.querySelectorAll(".divbox");
+  	bottonb1s[index].onclick = function () {
+  		container.removeChild(divboxs[index]);
+  	}
+  })
+}
+close();
+
+var confirms = document.querySelectorAll('bottonb2');
+confirms.forEach(function () {
+	confirms.onclick = function () {
+		var xml = new XMLHttpRequest();
+    xml.open("POST", "http://", true);
+    xml.withCredentials = true;
+    xml.onreadystatechange = function () {
+    	if (xml.readyState == 4 && xml.status == 200) {
+    		var text = JSON.parse(xml.responseText);
+    		if (text.errorcode ==0) {
+
+    		}else if (text.errorcode == 100) {
+
+    		}
+    	}
+    }
+	};
+	xml.send(null);
+})
