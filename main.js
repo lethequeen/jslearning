e004c8a6f24b53b395ca03d7df79388592f602bf
pigyun <814728667@qq.com>
2018-01-16 17:37:20 +0800

    alert

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 requery.config ({
 	paths : {
-		jquery : 'jquery-3.2.1.min'
+		jquery : 'jquery-3.2.1.min',
+		jqueryUI: 'http://code.jquery.com/ui/1.10.4/jquery-ui'
 	}
 });
 
 require (['jquery','window'], function ($, w) {
 	$("#a").click(function () {
-		new w.Window().alert("welcome !");
+		new w.Window().alert({
+			title: "提示",
+ 			content:"welcome !", 
+			handler4AlertBtn: function () {
+			  alert("you click the alert button");
+		  }
+		  handler4ClosetBtn: function () {
+		  	alert("you click the close button");
+		  }
+			width:300,
+			height:150,
+			y:50,
+			hasCloseBtn: true,
+			skinClassName: "window_skin_a",
+			text4AlertBtn: "OK",
+			dragHandle: ".window_header"
+		});
 	});
 });
